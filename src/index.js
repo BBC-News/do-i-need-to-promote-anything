966cbd5f08919a585051d1798dcd8c7f9923537d
Joseph Wynn <joseph.wynn@bbc.co.uk>
2017-04-18 14:38:41 +0100

    Preserve component order in output

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Promise = require('bluebird')
 const fs = Promise.promisifyAll(require('fs'))
 const { map, prop, uniq } = require('lodash/fp')
+const chalk = require('chalk')
 
 const { analyseVersions, extractTemplateFromUrl } = require('./morph')
 
@@ -12,6 +13,8 @@ module.exports = function doINeedToPromoteAnythingInThis (config) {
     .then(map(extractTemplateFromUrl))
     .then(uniq)
     .then(map(analyseVersions))
+    .all()
+    .then(map(outputVersions))
     .catch(SyntaxError, configCouldNotBeParsed)
     .error(configCouldNotBeLoaded)
 }
@@ -22,4 +25,18 @@ function configCouldNotBeLoaded () {
 
 function configCouldNotBeParsed (e) {
   throw new Error(`The config file is not valid JSON (${e.message})`)
+}
+
+function outputVersions ({ name, versions }) {
+  if (versions.int === versions.live) {
+    console.log(chalk.green(`🎉  ${chalk.bold(name)} is up-to-date on all environments`))
+  }
+
+  if (versions.int === versions.test && versions.test !== versions.live) {
+    console.log(chalk.cyan(`💁  ${chalk.bold(name)} needs to be promoted to live (${chalk.bold(versions.live)} -> ${chalk.bold(versions.test)})`))
+  }
+
+  if (versions.int !== versions.test) {
+    console.log(chalk.yellow(`🙋  ${chalk.bold(name)} needs to be promoted to test (${chalk.bold(versions.test)} -> ${chalk.bold(versions.int)})`))
+  }
 }
--- src/morph.js
+++ src/morph.js
@@ -1,31 +1,23 @@
 const Promise = require('bluebird')
 const HttpClient = require('bbc-http-client')
-const chalk = require('chalk')
 const { prop } = require('lodash/fp')
 
 const client = new HttpClient({ timeout: 10000 })
 
 module.exports.extractTemplateFromUrl = (url) => url.split(/\/(data|view)\//)[2].split(/(\/|\?)/)[0]
 
-module.exports.analyseVersions = (template) => {
-  Promise.all([
-    getIntVersion(template),
-    getTestVersion(template),
-    getLiveVersion(template)
-  ]).then(([vInt, vTest, vLive]) => {
-    if (vInt === vLive) {
-      console.log(chalk.green(`🎉  ${chalk.bold(template)} is up-to-date on all environments`))
-    }
-
-    if (vInt === vTest && vTest !== vLive) {
-      console.log(chalk.cyan(`💁  ${chalk.bold(template)} needs to be promoted to live (${chalk.bold(vLive)} -> ${chalk.bold(vTest)})`))
-    }
-
-    if (vInt !== vTest) {
-      console.log(chalk.yellow(`🙋  ${chalk.bold(template)} needs to be promoted to test (${chalk.bold(vTest)} -> ${chalk.bold(vInt)})`))
-    }
-  })
-}
+module.exports.analyseVersions = (template) => Promise.all([
+  getIntVersion(template),
+  getTestVersion(template),
+  getLiveVersion(template)
+]).then(([vInt, vTest, vLive]) => ({
+  name: template,
+  versions: {
+    int: vInt,
+    test: vTest,
+    live: vLive
+  }
+}))
 
 function getIntVersion (template) {
   return getVersion(`https://manager.morph.int.api.bbci.co.uk/module/${template}/environmentVersion`, template)
